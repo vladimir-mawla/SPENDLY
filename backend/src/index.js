6aeace338b06778f99eabce88f9d7ddd4ff68c81
vladimir <vladimirmawla2@gmail.com>
2022-08-22 20:33:37 +0300

    BACKEND: Editing databse and index files

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,22 +1,10 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const cors = require('cors')
-const path = require('path')
-const dotenv = require('dotenv')
-const  connectDB = require('./config/db')
+const http = require("http");
+const app = require("./app");
+const server = http.createServer(app);
 
-dotenv.config({ path: path.join(__dirname, `../.env.${process.env.NODE_ENV ? process.env.NODE_ENV : 'development'}`)})
+const port = process.env.PORT || 5000;
 
-const port = process.env.PORT || 5000
-
-connectDB()
-
-const app = express()
-
-app.use(cors())
-app.use(express.json())
-
-// Routes
-// app.use('/api', require('./api/routes'))
-
-app.listen(port, () => console.log(`Server running on port ${port}`))
+// server listening 
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
