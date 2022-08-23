5f24c5f19e2cbb0c12d9147f283f9a3317ff3ba5
vladimir <vladimirmawla2@gmail.com>
2022-08-23 11:37:24 +0300

    BACKEND: Creating user routes

---
+++ backend/src/api/routes/userRoutes.js
@@ -0,0 +1,12 @@
+// importing user context
+const User = require("../models/userModel");
+
+// Register
+app.post("/register", (req, res) => {
+// our register logic goes here...
+});
+
+// Login
+app.post("/login", (req, res) => {
+// our login logic goes here
+});
