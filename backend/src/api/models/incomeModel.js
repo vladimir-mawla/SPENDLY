fd4464e0e56c9a66ef6e5cfec7f671e25c761196
vladimir <vladimirmawla2@gmail.com>
2022-08-24 17:00:29 +0300

    BACKEND: Creating income model

---
+++ backend/src/api/models/incomeModel.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose')
+
+const incomeSchema = mongoose.Schema(
+    {
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            required: true,
+            ref: 'User',
+            immutable: true,
+        },
+        type: {
+            type: String,
+            required: [true, 'Please add a type'],
+            lowercase: true,
+        },
+        amount: {
+            type: Number,
+            required: [true, 'Please add an amount'],
+            min: 1,
+        },
+        date: {
+            type: Date,
+            default: () => Date.now(),
+            required: false,
+        },
+    },
+    {
+        timestamps: true,
+    }
+)
+
+
+module.exports = mongoose.model('Income', incomeSchema)
