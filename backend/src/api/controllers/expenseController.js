580a86de3cf9218cf6c7e1159d52ebd9f7e5d0f1
vladimir <vladimirmawla2@gmail.com>
2022-08-24 17:18:20 +0300

    BACKEND: Creating get expenses api

--- backend/src/api/controllers/expenseController.js
+++ backend/src/api/controllers/expenseController.js
@@ -4,3 +4,12 @@ const json2csv = require("json2csv").parse
 const fs = require("fs")
 const { validateImport } = require("../helpers/common")
 const Expense = require("../models/expenseModel")
+
+// @desc    Get expenses
+// @route   GET /api/expenses
+// @access  Private
+const getExpenses = asyncHandler(async (req, res) => {
+  const expenses = await Expense.find({ user: req.user.id })
+
+  res.status(200).json(expenses)
+})
