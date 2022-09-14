b3a1bd0646629c1ec4287cd09fbf27ffb142e7d5
vladimir <vladimirmawla2@gmail.com>
2022-09-14 10:31:34 +0300

    Removing files to replace them with new local files

--- frontend/src/helpers/axios.js
+++
@@ -1,4 +0,0 @@
-import axios from "axios"
-export default axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL || "http://localhost:5000/api",
-})
--- frontend/src/validators/documentValidator.js
+++
@@ -1,8 +0,0 @@
-import * as yup from "yup"
-
-const documentSchema = yup.object({
-  notes: yup.string("Enter your notes"),
-  date: yup.date("Enter the date"),
-})
-
-export { documentSchema }
